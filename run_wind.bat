0a3bc134ef7cf4cc6bbf254c2d17d721683b3472
wanjian <wanjian@MGJwanjian.local>
2017-06-22 14:43:46 +0800

    no message

--- run_wind.bat
+++ run_wind.bat
@@ -1,5 +1,7 @@
 
-# windows
-javac -cp src:lib/*  src/com/wanjian/domain/AutoBandDomain.java   -d class
 
-java -cp class:lib/*  com.wanjian.domain.AutoBandDomain
+javac -encoding utf-8 -cp src:lib/*  src/com/wanjian/domain/AutoBandDomain.java   -d class
+
+
+
+ java -cp class:lib/*  com.wanjian.domain.AutoBandDomain
